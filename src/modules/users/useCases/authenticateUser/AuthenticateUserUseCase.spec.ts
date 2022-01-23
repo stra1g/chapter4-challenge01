6f2a4f2b55f58ec77a8a21b512bf1f107b2b6c4b
Luis Vitor <luis.vitor@yazo.com.br>
2022-01-22 23:15:36 -0300

    test: AuthenticateUserUseCase

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import { InMemoryUsersRepository } from '../../repositories/in-memory/InMemoryUsersRepository';
 import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
 import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
+import { IncorrectEmailOrPasswordError } from './IncorrectEmailOrPasswordError';
 
 let createUserUseCase: CreateUserUseCase;
 let authenticateUserUseCase: AuthenticateUserUseCase;
@@ -30,5 +31,31 @@ describe("Authenticate user", () => {
     });
 
     expect(tokenInfo).toHaveProperty('token');
+  });
+
+  it("should not be to authenticate a user with invalid email", () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({ 
+        email: 'invalidEmail@email.com', 
+        password: '12345'
+      });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
+  });
+
+  it("should not be to authenticate a user with invalid email", () => {
+    expect(async () => {
+      const user = {
+        email: "user@test.com",
+        password: "1234",
+        name: "User test",
+      };
+  
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({ 
+        email: user.email, 
+        password: 'wrongPassword'
+      });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
   });
 })
